de967562ce0aec46632efec11ffac37c1a0ecc24
oherik <erik.ohrn@outlook.com>
2015-02-09 11:57:08 +0100

    Lets try again

---
+++ Lab2v2/testframe/SortedLinkedCollection.java
@@ -0,0 +1,17 @@
+
+
+//import java.util.List;
+
+import testSortCol.CollectionWithGet;
+//import testSortCol.TestMapWithCounter.TestMapEntry;
+import datastructures.*;
+public class SortedLinkedCollection<E extends Comparable<? super E>> extends LinkedCollection<E> implements CollectionWithGet<E> {
+
+	@Override
+	public E get(E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
