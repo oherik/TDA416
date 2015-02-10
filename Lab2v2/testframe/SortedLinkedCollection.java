a88f57d14a2aec5115f6b26b0ff9fe7a48a040b4
oherik <erik.ohrn@outlook.com>
2015-02-10 17:08:01 +0100

    Added broken get method

--- Lab2v2/testframe/SortedLinkedCollection.java
+++ Lab2v2/testframe/SortedLinkedCollection.java
@@ -19,7 +19,11 @@ public class SortedLinkedCollection<E extends Comparable<? super E>> extends
 	}
 
 	/**
-	 * Initializer function for getRecursive.
+	 * Find the first occurrence of an element in the list that is equal to the
+	 * argument e with respect to its natural order. I.e. e.compateTo(element)
+	 * is 0. It compares the element in the argument with the one in the entry
+	 * and goes through all the entries until the element is found.
+	 * 
 	 * 
 	 * @param e
 	 *            The element which is to be found
@@ -32,35 +36,25 @@ public class SortedLinkedCollection<E extends Comparable<? super E>> extends
 	public E get(E e) {
 		if (e == null)
 			throw new NullPointerException();
-		return getRecursive(e, head);	
-	}
+
+			Entry entry = head;
+			while(entry!=null){
+				if(e.compareTo(entry.element) == 0)
+					return entry.element;
+			else if (e.compareTo(entry.element) > 0)
+					return null;
+				entry = entry.next;
+			}
+		
+		return null;
+				
+			
+	}//TODO: get ä paj
+
+	
 
 	/**
-	 * Find the first occurrence of an element in the list that is equal to the
-	 * argument e with respect to its natural order. I.e. e.compateTo(element)
-	 * is 0. It compares the element in the argument with the one in the entry
-	 * and goes through all the entries until the element is found.
-	 * 
-	 * @param e
-	 *            The element which is to be found
-	 * @param entry
-	 *            The current entry in which to compare e to the element in the
-	 *            entry.
-	 * @return The first element in the list that satisfies e.compareTo(e')==0.
-	 *         Returns null if none is found.
-	 */
-	private E getRecursive(E e, Entry entry){
-		if(entry==null)
-			return null;
-		else if (e.compareTo(entry.element) == 0)
-			return entry.element;
-		else if (e.compareTo(entry.element) > 0)  // since it's	sorted we can abort early
-			return null;
-		return getRecursive(e, entry.next);	
-	}
-
-	/**
-	 * Adds a new element to the list, in a sorted order
+	 * Adds a new element to the list, in a sorted order according to compareTo.
 	 * 
 	 * @param element
 	 *            The element which is to be added
@@ -72,27 +66,15 @@ public class SortedLinkedCollection<E extends Comparable<? super E>> extends
 	public boolean add(E element) {
 		if (element == null)
 			throw new NullPointerException();
-		if (head == null || element.compareTo(head.element) < 0)
-			head = new Entry(element, null);
-		else
-			addSorted(element, head);
+		else if (head == null || element.compareTo(head.element) < 0)
+			head = new Entry(element, head);
+		else{
+			Entry entry = head;
+			while(entry.next!=null && element.compareTo(entry.next.element) >= 0)
+				entry = entry.next;				
+			
+			entry.next = new Entry(element, entry.next);
+		}
 		return true;
 	} // add
-
-	/**
-	 * Compares a given element to one in a given entry and adds it before if
-	 * it's smaller according to compareTo.
-	 * 
-	 * @param element
-	 *            The element which is to be added
-	 * @param entry
-	 *            The entry whose element it compares to
-	 */
-	private void addSorted(E element, Entry entry) {
-		if (entry.next != null && element.compareTo(entry.element) < 0)
-			addSorted(element, entry.next);
-		else
-			entry.next = new Entry(element, entry.next);
-	}
-
 }
