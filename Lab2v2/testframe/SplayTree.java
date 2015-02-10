ca5203353bb4e8a920f546e76bc95656408d73ed
oherik <erik.ohrn@outlook.com>
2015-02-10 11:18:11 +0100

    Created SplayTree and added basics imports, etc.

---
+++ Lab2v2/testframe/SplayTree.java
@@ -0,0 +1,14 @@
+import datastructures.BinarySearchTree;
+import datastructures.LinkedCollection;
+import testSortCol.CollectionWithGet;
+
+public class SplayTree<E extends Comparable<? super E>> extends
+BinarySearchTree<E> implements CollectionWithGet<E>{
+
+	@Override
+	public E get(E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
