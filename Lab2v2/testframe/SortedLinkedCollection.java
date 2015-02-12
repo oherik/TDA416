b9efb1e6920acb67a3345aa0754955ade16c184a
oherik <erik.ohrn@outlook.com>
2015-02-12 08:56:35 +0100

    Mer kommentarer. Det verkar som att while blir snabbare än en rekursiv funktion pga hur stacken fungerar, så vi kör på det tycker jag

--- Lab2v2/testframe/SortedLinkedCollection.java
+++ Lab2v2/testframe/SortedLinkedCollection.java
@@ -1,9 +1,11 @@
-/**
- * A subclass to LinkedCollection. It is in all appearances identical, 
- * except that it also has a "get" method and the add method sorts it.
+//Grupp 7 - Erik Öhrn & Paula Eriksson Imable
+
+ /**
+ * A subclass to LinkedCollection. It is in all appearances identical,  
+ * except that it also has a "get" method and the add method sorts the list.
  *
  * 
- * @author Erik Öhrn
+ * @author Erik Öhrn & Paula Eriksson Imable
  * @version 0.4
  */
 
@@ -36,14 +38,14 @@ public class SortedLinkedCollection<E extends Comparable<? super E>> extends
 	public E get(E e) {
 		if (e == null)
 			throw new NullPointerException();
-			Entry entry = head;
-			int compare = 0;
-			while(entry!=null	&&	compare>=0){	
-				compare = e.compareTo(entry.element);
-				if(compare == 0)
-					return entry.element;
-				entry = entry.next;
-			}
+		Entry entry = head;
+		int compare = 0;
+		while(entry!=null	&&	compare>=0){	//Lika bra att bryta när compare under 0 eftersom listan nu är ordnad
+			compare = e.compareTo(entry.element);
+			if(compare == 0)
+				return entry.element;
+			entry = entry.next;
+		}
 		return null;			
 	}
 
@@ -63,7 +65,7 @@ public class SortedLinkedCollection<E extends Comparable<? super E>> extends
 		if (element == null)
 			throw new NullPointerException();
 		if (head == null || element.compareTo(head.element) < 0)
-			head = new Entry(element, head);
+			head = new Entry(element, head);					//I fallet då head är null kommer next också bli null.
 		else{
 			Entry entry = head;
 			while(entry.next!=null && element.compareTo(entry.next.element) >= 0)
