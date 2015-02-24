85736383c664da5acfc2c8f9743ae4ab560fd81f
oherik <erik.ohrn@outlook.com>
2015-02-24 17:19:34 +0100

    Addedge

--- Lab3/lab3.www/DirectedGraph.java
+++ Lab3/lab3.www/DirectedGraph.java
@@ -1,14 +1,31 @@
 import java.util.*;
+//Efterföljarlista?
+
 
 public class DirectedGraph<E extends Edge> {
-
+	private Map<Integer, LinkedList<E>> nodeMap;
+	//private LinkedList<E>[] nodeList;
 
 	public DirectedGraph(int noOfNodes) {
-		;
+		nodeMap = new HashMap<Integer, LinkedList<E>>();	
+		//nodeList = new LinkedList[noOfNodes];
+		//for(int i = 0; i<noOfNodes; i++)
+			//nodeList[i] = new LinkedList<E>();
+		
 	}
 
 	public void addEdge(E e) {
-		;
+		int source = e.getSource();
+	/*	LinkedList<E> list;
+		for(int i = 0; !nodeList[i].isEmpty(); i++){
+			list = nodeList[i];
+			if(list.get(0).getSource()==e.getSource()
+				list.add(e);
+			}
+		}
+		*/
+	nodeMap.putIfAbsent(source, new LinkedList<E>());
+	nodeMap.get(source).add(e);			
 	}
 
 	public Iterator<E> shortestPath(int from, int to) {
---
+++ Lab3/lab3.www/QueueElement.java
@@ -0,0 +1,13 @@
+
+public class QueueElement {
+	private NodeObject to;
+	private boolean known;
+	private double cost;
+	private NodeObject path;
+	
+	public QueueElement(NodeObject to){
+		this.to = to;
+	}
+	
+	
+}
