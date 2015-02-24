9016a9084d1717c8a7494053c103bd2975ac46b8
oherik <erik.ohrn@outlook.com>
2015-02-24 15:36:25 +0100

    Added pseudocode

--- Lab3/lab3.www/DirectedGraph.java
+++ Lab3/lab3.www/DirectedGraph.java
@@ -1,4 +1,15 @@
-
+/**
+ *lägg (startnod, 0, tom väg) i en p-kö
+	while kön inte är tom
+		(nod, cost, path) = första elementet i p-kön
+		if nod ej är besökt
+			if nod är slutpunkt returnera path
+			else
+				markera nod besökt
+				for every v on EL(nod)
+					if v ej är besökt
+						lägg in nytt köelement för v i p-kön
+ */
 import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
