db4205318a6bd55675547eb7c6ab59ab418c8a52
oherik <erik.ohrn@outlook.com>
2015-02-25 10:17:57 +0100

    Testar med ett fält

--- Lab3/lab3.www/DirectedGraph.java
+++ Lab3/lab3.www/DirectedGraph.java
@@ -1,46 +1,41 @@
 import java.util.*;
+
 //Efterföljarlista?
 
-
 public class DirectedGraph<E extends Edge> {
-	private Map<Integer, LinkedList<E>> nodeMap;
-	//private LinkedList<E>[] nodeList;
+	// private Map<Integer, LinkedList<E>> nodeMap;
+	private LinkedList<E>[] nodeList;
 
 	public DirectedGraph(int noOfNodes) {
-		nodeMap = new HashMap<Integer, LinkedList<E>>();	
-		//nodeList = new LinkedList[noOfNodes];
-		//for(int i = 0; i<noOfNodes; i++)
-			//nodeList[i] = new LinkedList<E>();
-		
-		
+		// nodeMap = new HashMap<Integer, LinkedList<E>>();
+		// nodeList = new LinkedList[noOfNodes];
+		for (int i = 0; i < noOfNodes; i++)
+			nodeList[i] = new LinkedList<E>();
+
 		/*
-		Revelation! NodeTable.java gör om alla nodnamn till heltal. Är det så att de är från 0-nånting? I så fall blir det mycket snabbare, och smidigare med ett fält eller en ArrayList.
-		
-		
-		*/
+		 * Revelation! NodeTable.java gör om alla nodnamn till heltal. Är det så
+		 * att de är från 0-nånting? I så fall blir det mycket snabbare, och
+		 * smidigare med ett fält eller en ArrayList. Testar det.
+		 */
 	}
 
 	public void addEdge(E e) {
-		int source = e.getSource();
-	/*	LinkedList<E> list;
-		for(int i = 0; !nodeList[i].isEmpty(); i++){
-			list = nodeList[i];
-			if(list.get(0).getSource()==e.getSource()
-				list.add(e);
-			}
-		}
-		*/
-	nodeMap.putIfAbsent(source, new LinkedList<E>());
-	nodeMap.get(source).add(e);			
+		/*
+		 * Ska man bara ta med båden om ingen kortare redan finns? Eller
+		 * tillåtervi flera bågar mellan samma noder?
+		 */
+		// int source = e.getSource();
+		nodeList[e.getSource()].add(e);
+		// nodeMap.putIfAbsent(source, new LinkedList<E>());
+		// nodeMap.get(source).add(e);
 	}
 
 	public Iterator<E> shortestPath(int from, int to) {
 		return null;
 	}
-		
+
 	public Iterator<E> minimumSpanningTree() {
 		return null;
 	}
 
 }
-  
