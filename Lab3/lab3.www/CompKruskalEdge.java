80ee26520cfecb1d3c5415d0956e84314182fbf8
oherik <erik.ohrn@outlook.com>
2015-02-26 16:49:21 +0100

    Framework for CompKruskalEdge

--- Lab3/lab3.www/CompKruskalEdge.java
+++ Lab3/lab3.www/CompKruskalEdge.java
@@ -1,8 +1,22 @@
+import java.util.LinkedList;
+
 /**
  * 
  * @author Erik
  *
  */
-public class CompKruskalEdge {
+public class CompKruskalEdge<E extends Edge> {
+	
+	public LinkedList<E> getSmall(){
+		return null;
+	}
+
+	public LinkedList<E> getLarge(){
+		return null;
+	}
+	
+	public boolean isLoop(E e, LinkedList<E>[] list){
+		return false;
+	}
 
 }
