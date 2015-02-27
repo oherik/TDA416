3927a58102717e29778db070d864ee62818469a3
oherik <erik.ohrn@outlook.com>
2015-02-27 08:37:10 +0100

    Måste implementera compare på nåt sätt

--- Lab3/lab3.www/CompKruskalEdge.java
+++ Lab3/lab3.www/CompKruskalEdge.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 import java.util.LinkedList;
 
 /**
@@ -5,13 +6,19 @@ import java.util.LinkedList;
  * @author Erik
  *
  */
-public class CompKruskalEdge<E extends Edge> {
-	
+public class CompKruskalEdge<E extends Edge> implements Comparator{
+	//Eller extends BusEdge implements Comparable
 	
 	public int getStartIndex(E e, LinkedList<E>[] list){ //TODO ska man ha double linked istället?
 		return 0;
 	}
 	public int getDestIndex(E e, LinkedList<E>[] list){
+		return 0;
+	}
+	
+	@Override
+	public int compare(Object arg0, Object arg1) {
+		// TODO Auto-generated method stub
 		return 0;
 	}
 	
